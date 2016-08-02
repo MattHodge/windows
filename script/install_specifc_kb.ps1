da00baa205f31103f13c9fbf7b2405bda65f1573
Matthew Hodgkins <matthodge@gmail.com>
2016-08-02 08:26:32 +0200

    installing kb's first

--- script/install_specifc_kb.ps1
+++ script/install_specifc_kb.ps1
@@ -1,34 +1,64 @@
-$wuInstallExe = Join-Path "$($env:windir)\SYSTEM32" 'WUInstallAMD64.exe'
+param (
+    $KB,
+    $KBFileName,
+    $DownloadURI
+)
 
-if (!(Test-Path -Path $wuInstallExe))
+# https://download.microsoft.com/download/D/B/1/DB1F29FC-316D-481E-B435-1654BA185DCF/Windows8.1-KB2919355-x64.msu
+
+# 'http://192.168.2.115:8080/HTTPServer/Windows8.1-KB2919355-x64.msu'
+
+# $KB = 'KB2919355'
+# $KBFileName = 'Windows8.1-KB2919355-x64.msu'
+
+$hotFixes = Get-HotFix
+
+if ($hotfixes.HotFixID.Contains($KB))
 {
-    Invoke-WebRequest -UseBasicParsing -Uri 'https://dl.dropboxusercontent.com/u/727435/Tools/WUInstallAMD64.exe' -OutFile $wuInstallExe
+  Write-Output "$($KB) is already installed."
+  exit 0
 }
 
 $schTaskName = Get-Random
 $scriptName = "$($schTaskName).ps1"
 
 [scriptblock]$schTaskScript = {
+    param (
+        $KB,
+        $KBFileName,
+        $DownloadURI
+    )
     $npipeClient = new-object System.IO.Pipes.NamedPipeClientStream($env:ComputerName, 'task', [System.IO.Pipes.PipeDirection]::Out)
     $npipeclient.connect()
     $pipeWriter = new-object System.IO.StreamWriter($npipeClient)
     $pipeWriter.AutoFlush = $true
-    C:\WINDOWS\SYSTEM32\WUInstallAMD64.exe /install /autoaccepteula /silent | foreach-object {} {$pipewriter.writeline($_)} {
-        $pipewriter.writeline("SCHEDULED_TASK_DONE: $LastExitCode")
-        $pipewriter.dispose()
-        $npipeclient.dispose()
+    
+    $updatePath = Join-Path "$($env:TEMP)" $KBFileName
+
+    if (!(Test-Path -Path $updatePath))
+    {
+        $pipewriter.writeline("$(get-date -Format s) Downloading update to $($updatePath)")
+        Invoke-WebRequest -UseBasicParsing -Uri $DownloadURI -OutFile $updatePath
+        $pipewriter.writeline("$(get-date -Format s) Finished Downloading Update")
     }
-}
+
+    $pipewriter.writeline("$(get-date -Format s) Starting update install from $($updatePath)")
+    Start-Process -FilePath 'wusa.exe' -ArgumentList "$($updatePath) /quiet /norestart" -Wait -NoNewWindow
+    $pipewriter.writeline("$(get-date -Format s) Update installation complete")
+
+    $pipewriter.writeline("SCHEDULED_TASK_DONE: $LastExitCode")
+    $pipewriter.dispose()
+    $npipeclient.dispose()
+}.GetNewClosure()
 
 Write-Output "Creating Script File"
 $scriptPath = Join-Path $env:TEMP $scriptName
 Set-Content -Path $scriptPath -Value $schTaskScript -Force
 
 Write-Output "Creating Scheduled Task"
-Start-Process -FilePath 'schtasks' -ArgumentList "/create /tn $($schTaskName) /ru vagrant /rp vagrant /sc once /st 00:00 /sd 01/01/2005 /f /tr ""powershell -executionpolicy unrestricted -File '$($scriptPath)'""" -Wait -NoNewWindow
+Start-Process -FilePath 'schtasks' -ArgumentList "/create /tn $($schTaskName) /ru vagrant /rp vagrant /sc once /st 00:00 /sd 01/01/2005 /f /tr ""powershell -executionpolicy unrestricted -File '$($scriptPath)' $KB $KBFileName $DownloadURI""" -Wait -NoNewWindow
 
 Start-Sleep -Seconds 5
-
 
 Write-Output "$(get-date -Format s) Running Scheduled Task"
 try
@@ -84,7 +114,7 @@ finally
     $npipeserver.dispose()
 
     Write-Output "$(get-date -Format s) Removing Scheduled Task"
-    Start-Process -FilePath 'schtasks' -ArgumentList "SchTasks /Delete /TN $($schTaskName)"
+    Start-Process -FilePath 'schtasks' -ArgumentList "/Delete /TN $($schTaskName) /F"
 
     $host.setshouldexit($exit_code)
 }
